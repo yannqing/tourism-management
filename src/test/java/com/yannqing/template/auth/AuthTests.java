de8545826885379f63d6012668894371611ba651
yanqing <67121851@qq.com>
2025-02-03 00:55:18 +0800

    feat&add: 新增获取个人信息接口

--- src/test/java/com/yannqing/template/auth/AuthTests.java
+++ src/test/java/com/yannqing/template/auth/AuthTests.java
@@ -1,20 +1,20 @@
-package com.yannqing.template.auth;
-
-import jakarta.annotation.Resource;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@SpringBootTest
-class AuthTests {
-
-    @Resource
-    private PasswordEncoder passwordEncoder;
-
-    @Test
-    void testPassword() {
-        String encodePassword = passwordEncoder.encode("123456");
-        System.out.println(encodePassword);
-    }
-
-}
+//package com.yannqing.template.auth;
+//
+//import jakarta.annotation.Resource;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@SpringBootTest
+//class AuthTests {
+//
+//    @Resource
+//    private PasswordEncoder passwordEncoder;
+//
+//    @Test
+//    void testPassword() {
+//        String encodePassword = passwordEncoder.encode("123456");
+//        System.out.println(encodePassword);
+//    }
+//
+//}
